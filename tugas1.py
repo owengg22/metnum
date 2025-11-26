cefb8e5a29c4be73e3929028188357c4c2380b6e
owengg22 <orleansgaming@gmail.com>
2025-11-26 14:01:21 +0700

    rev1

--- tugas1.py
+++ tugas1.py
@@ -3,31 +3,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data = pd.read_csv("ecg.csv", header=None)
+data = pd.read_csv("abdomen1.txt", header=None)
+y = data.iloc[:, 0].values   
 
-x = data.iloc[:, 0].values
-y = data.iloc[:, 1].values
+t = np.arange(len(y))       
 
-signal_length = np.sqrt(x**2 + y**2)
-t = np.arange(len(signal_length))
 
-order = 5   
-
-coeffs = np.polyfit(t, y, order)        
-baseline = np.polyval(coeffs, t)      
+order = 5
+coeffs = np.polyfit(t, y, order)
+baseline = np.polyval(coeffs, t)
 y_detrended = y - baseline
 
 
-Sr = np.sum((y - baseline) ** 2)        
-St = np.sum((y - np.mean(y)) ** 2)     
-r2 =  1 - Sr / St
+Sr = np.sum((y - baseline) ** 2)
+St = np.sum((y - np.mean(y)) ** 2)
+r2 = 1 - Sr / St
 
 print("Order polynomial =", order)
 print("Koefisien regresi =", coeffs)
 print("Nilai r^2 =", r2)
 
 
-plt.figure(figsize=(14, 5))
+plt.figure(figsize=(13, 4))
 plt.plot(t, y, label='Sinyal ECG asli')
 plt.plot(t, baseline, label='Baseline (Polynomial)', linewidth=2)
 plt.title(f"Sinyal ECG & Baseline (Order {order}) - r² = {r2:.4f}")
@@ -35,9 +32,9 @@ plt.legend()
 plt.grid()
 plt.show()
 
-plt.figure(figsize=(14, 5))
+plt.figure(figsize=(13, 4))
 plt.plot(t, y_detrended, label='Sinyal setelah koreksi baseline')
-plt.title("Sinyal FECG Detrended")
+plt.title("Sinyal ECG Detrended")
 plt.legend()
 plt.grid()
 plt.show()
