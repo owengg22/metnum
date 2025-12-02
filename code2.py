43ed2511f0441a90a719a0ad3771e8592ed89e61
owengg22 <orleansgaming@gmail.com>
2025-12-02 16:52:34 +0700

    harusnya fix  terakhir pls malas revisi

--- code2.py
+++ code2.py
@@ -2,15 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 data = pd.read_csv("FetalECG.txt", sep="\t", header=None)
 
 t = data.iloc[:, 0].values      
 y = data.iloc[:, 1].values      
 
-# segment untuk di analisis
 start = 1
-end = 1200 
+end = 1200
 t = t[start:end]
 y = y[start:end]
 
@@ -18,9 +16,7 @@ order = 5
 
 coeffs = np.polyfit(t, y, order)
 baseline = np.polyval(coeffs, t)
-
 y_detrended = y - baseline
-
 
 Sr = np.sum((y - baseline) ** 2)
 St = np.sum((y - np.mean(y)) ** 2)
@@ -30,18 +26,28 @@ print("Order polynomial =", order)
 print("Koefisien regresi =", coeffs)
 print("Nilai r^2 =", r2)
 
-plt.figure(figsize=(13,4))
+
+plt.figure(figsize=(13,8))
+
+plt.subplot(3,1,1)
 plt.plot(t, y, label="Original Fetal ECG")
+plt.title("Original Fetal ECG")
+plt.legend()
+plt.grid(True)
+
+plt.subplot(3,1,2)
+plt.plot(t, y, label="Original")
 plt.plot(t, baseline, linewidth=2, label=f"Baseline Polyfit (Order {order})")
 plt.title(f"Baseline Fitting (order={order}) - R²={r2:.4f}")
 plt.legend()
-plt.grid()
-plt.show()
+plt.grid(True)
 
-# baseline removal
-plt.figure(figsize=(13,4))
+
+plt.subplot(3,1,3)
 plt.plot(t, y_detrended, label="Detrended Fetal ECG")
 plt.title("Detrended ECG")
 plt.legend()
-plt.grid()  
+plt.grid(True)
+
+plt.tight_layout()
 plt.show()
--- tugas1.py
+++ tugas1.py
@@ -2,20 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 data = pd.read_csv("abdomen1.txt", header=None)
 y = data.iloc[:, 0].values
 
+#segment analisis
 start = 1
-end = 1000 #max 20000
+end = 1000   
 y = y[start:end]
 
+t = np.arange(len(y))
 
-t = np.arange(len(y))       
 
 order = 5
 coeffs = np.polyfit(t, y, order)
 baseline = np.polyval(coeffs, t)
 y_detrended = y - baseline
+
 
 Sr = np.sum((y - baseline) ** 2)
 St = np.sum((y - np.mean(y)) ** 2)
@@ -25,17 +28,28 @@ print("Order polynomial =", order)
 print("Koefisien regresi =", coeffs)
 print("Nilai r^2 =", r2)
 
-plt.figure(figsize=(13, 4))
-plt.plot(t, y, label='Sinyal ECG asli')
-plt.plot(t, baseline, label='Baseline (Polynomial)', linewidth=2)
-plt.title(f"Sinyal ECG & Baseline (Order {order}) - r² = {r2:.4f}")
-plt.legend()
-plt.grid()
-plt.show()
+plt.figure(figsize=(13,8))
 
-plt.figure(figsize=(13, 4))
-plt.plot(t, y_detrended, label='Sinyal setelah koreksi baseline')
-plt.title("Sinyal ECG Detrended")
+plt.subplot(3,1,1)
+plt.plot(t, y, label="Original ECG")
+plt.title("Original ECG") 
 plt.legend()
-plt.grid()
+plt.grid(True)
+
+
+plt.subplot(3,1,2)
+plt.plot(t, y, label="Original")
+plt.plot(t, baseline, linewidth=2, label=f"Baseline Polyfit (Order {order})")
+plt.title(f"Baseline Fitting (order={order}) - R²={r2:.4f}")
+plt.legend()
+plt.grid(True)
+
+
+plt.subplot(3,1,3)
+plt.plot(t, y_detrended, label="Detrended ECG")
+plt.title("Detrended ECG")
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
 plt.show()
